74706d01ed4cad3fe59568a555d80eb380b21c44
☆ Raka ☆ ™︻®╤───────═◍➤ <93072215+Bangsat-XD@users.noreply.github.com>
2021-11-07 11:16:42 +0700

    Update RAKA.py

--- RAKA.py
+++ RAKA.py
@@ -51,7 +51,7 @@ tBilall = ("%s-%s-%s-%s"%(hr, ha, op, ta))
 tgl = ("%s %s %s"%(ha, op, ta))
 bulan_ttl = {"01": "Januari", "02": "Februari", "03": "Maret", "04": "April", "05": "Mei", "06": "Juni", "07": "Juli", "08": "Agustus", "09": "September", "10": "Oktober", "11": "November", "12": "Desember"}
 
-logo = """ \033[1;91m                   
+def logo = """ \033[1;91m                   
                    `/ymMMMMMMMMmy/`
                `/ymMMMMMMMMMMMMMMmy/`
               /hMMMMMMMMMMMMMMMMMMMMMMh/
