f9a5e035737c98b5aa7f224766aae9e0f4f37f85
☆ Raka ☆ ™︻®╤───────═◍➤ <93072215+Bangsat-XD@users.noreply.github.com>
2021-11-10 10:34:51 +0700

    Update RAKA.py

--- RAKA.py
+++ RAKA.py
@@ -197,20 +197,7 @@ def menu():
 	elif Bilal == "0" or Bilal == "00":
 		os.system("rm -f login.txt")
 		exit("\033[1;97m token Removed")
-	else:
-		menu()
-                    cek = open("cookies").read()
-             except FileNotFoundError:
-                   ikeh_ikeh_kimochi()
-                   kontol()
-                   cek = input("\n\033[0;92m       [ \033[0;97mThis Tool Use Facebook Cookies \033[0;92m]\n\n\033[97m [\033[91m?\033[97m] Cookies \033[1;91m: \033[1;96m")
-                   print('\n\033[97m [\033[92m+\033[97m] \033[92mPleas Wait...')
-             cek = {"cookie":cek}
-             ismi = ses.get(mbasic.format("/me",verify=False),cookies=cek).content
-             if "mbasic_logout_button" in str(ismi):
-                     if "Apa yang Anda pikirkan sekarang" in str(ismi):
-                             with open("cookies","w") as f:
-                                     f.write(cek["cookie"])
+
                      else:
                            print("\033[1;97m[\033[1;94m•\033[1;97m] \033[00mUbah bahasa, harap tunggu\033[1;91m!!\033[00m")
                            try:
