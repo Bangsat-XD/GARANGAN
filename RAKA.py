5704fe3f68ac94fc54b093994adda5461c9d0bdf
☆ Raka ☆ ™︻®╤───────═◍➤ <93072215+Bangsat-XD@users.noreply.github.com>
2021-11-12 19:56:08 +0700

    Add files via upload

---
+++ RAKA.py
@@ -0,0 +1,124 @@
+{@`\`@ z@ `
+%{@ @ @Ax@ @Ax@I ?GAGAGAGAGAGAGA
+%{@ @ @ @ @ @ @ @ @KKK
+%{@ @ @ @ @ @ @ @ @ooo
+% @
+% z
+% @
+% @ z
+% K M @ @ ]
+%
+% z
+% @
+% @ z
+% K M @ @ ]
+%
+% @ k@ k@ k@ k@ k@ k@ k@
+% @ K @ @
+% @ @ @ @ @
+% @ @ @
+% @ @ @
+%
+% @~@
+% @~@p
+% @~@p
+% @~@p
+%
+% @~@ K M]
+% @~@ K
+% @~@p k@ k@ k@ k@ k@ k@ k@ k@ k@ k@ k@ 
+% z
+%@@@@ @ @L@ @ @ @n@ z
+%@@@@@@@@ M]
+%@@@@ @~@ @`@
+% @ z
+%@@@@ M]
+%
+% @~@ K M]
+% @~@ K
+% @~@ K
+% @~@ K
+% @~@ p
+%
+%
+% @@ M ]z
+% @ @ @ @N@} }z
+% K K M ]
+% K K M]
+% K M K ]
+%
+% m @~@ K M]
+% @~@ K m p m K M]
+% @~@Ml `l `l `l lM k@ k@ k@ ]]
+% @~@Ml @l @l lM k@ k@ ]]
+% m @~@  z@ k@ z@ k@ z@ k@ z@ k@ z@ k@ z@ k@ z@ k@ z@ k@ z@ k@ z@ k@ z@ k@ z@ 
+%
+%@@@@@@@@@@@@@
+% @ M]z
+% K M ]
+% M p ^ @
+%@@@@@@@@@@@@@@@@@@ p ^ GAGAGAGAGAGA
+%@@@@@@@@@@@@@@@@@@GAGAGAItItItItGAGAGA
+%@@@@@@@@@@@@@@@@@@GAGAGAGAGAGAGAGA
+%@@@@@@@@@@@@@@@@@@GAGAGAGAGAGA
+%@@@@@@@@@@@@@@@@@@GAGA
+%@@@@@@@@@@@@@@@@@@ p ^
+%@@@@@@@@@@@@@@@@@@GAGA
+%@@@@@@@@@@@@@@@@@@GAGAGAGA
+%@@@@@@@@@@@@@@@@@@GA
+%@@@@@@@@@@@@@@@@@@@@
+%@@@@@@@@@@@@@GNGLGLGLGOGVGVGNGOGVGVGVGVGNGLGLGLGOGVGVGVGVGVGVGVGVGVGVGNGO
+%@@@@@@@@@@@@@GMGNGLGOGMGVGVGMGMGVGVGVGVGMGNGLGOGMGVGVGVGVGVGVGVGVGVGVGMGM
+%@@@@@@@@@@@@@GMGQGLGPGRGLGLGTGMGNGSGLGLGTGMGVGMGRGOGNGSGLGLGSGLGOGNGLGPGRGLGLGO@@
+%@@@@@@@@@@@@@ p ^ GMGNGOGNGTGNGOGMGQGPGTGNGOGMGQGLGPGMGQGPGMGNGOGMGNGOGTGNGOGMGNGOGM
+%@@@@@@@@@@@@@GMGMGMGQGTGNGOGMGNGOGTGNGOGMGNGLGOGMGMGMGMGNGOGMGMGMGMGQGPGMGNGOGM
+%@@@@@@@@@@@@@GQGPGQGLGUGPGQGUGPGQGUGPGQGUGPGVGQGUGUGUGUGPGQGUGPGQGUGLGLGUGPGQGP@ p ^ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+%@@@@ p ^ GAGAGAGAGAGA@ p ^ A{@ p ^ @ p ^ p@ @ @ @ @KK@ @ @ @ @ @ @ p ^ @@ p ^ A{@ p ^ p ^ GAGAGAGAGAGA@
+% p ^ ``````````````````````````````````````````````````
+% p ^ p ^ @ @@@@@z@ p ^ Ax@ @Ax@I ?GAGAGAGAGAGAGA@ p ^
+% p ^ p ^ @ @@@@@z@ p ^ zaa K a ` @ p ^
+% p ^ p ^ @ @@@z@ p ^ @ @ @ p ^
+% p ^ p ^ @ @@z@ p ^ m @ p ^
+% p ^ p ^ @ @@@@z@ p ^ m @ p ^
+% p ^ ``````````````````````````````````````````````````]
+% @ M]z
+% K M ]
+% z
+%{`n@ @
+% K M zaa K K ]
+% @ K K z
+% M @ @ ]
+% z
+% @~@ M K k@ ]
+% M]
+% @ k@ z
+% @~@ m M p ^ p ^ @ @ @z@]
+% @ @~~@z
+% M @ ]
+% z
+% @~@ K M zaa K K a o m ~N ]K M]p  K M]
+% M K k@ ]K M ]
+%{`n@ @
+% K M zaa K K a a o m ~N ]@@@@@@{@ @ @
+% K M zaa K K a a o m ~N ]@@@@@@{@ @ @
+% K M zaa K K a a o m ~N ]@@@@@@{@ @
+% K M zaa K K a a o m ~N ]@@@@@@{@
+% M]
+% @ z
+% K M @` @ K ]
+% Mpo @ @ ]
+%
+% @ M]z
+% K M ]
+% @
+% z
+% @~@ M K k ]K M]
+% @ z
+% K M @` @ K ]
+% Mpo @ @ ]
+% z
+% @~@ K M zaa K K a ao m ~N ]K M]p  K M]
+% @ z
+% K M @` @ K ]
+% M p ^ p p ^ N p ^ @ @ ]
+% @ K K z
